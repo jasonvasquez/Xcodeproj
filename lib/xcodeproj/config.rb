96d6bc44dbb5b0ab1d6bc656dcf72b30c976bac5
Samuel Giddins <segiddins@segiddins.me>
2015-10-05 18:48:08 -0500

    [Config] Fix merging settings that have multiple entries in common

--- lib/xcodeproj/config.rb
+++ lib/xcodeproj/config.rb
@@ -276,8 +276,7 @@ module Xcodeproj
     def merge_attributes!(attributes)
       @attributes.merge!(attributes) do |_, v1, v2|
         v1, v2 = v1.strip, v2.strip
-        existing = v1.strip.shellsplit
-        existing.include?(v2) ? v1 : "#{v1} #{v2}"
+        (v2.shellsplit - v1.shellsplit).empty? ? v1 : "#{v1} #{v2}"
       end
     end
 
